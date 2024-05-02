e6f0568b8cab134414a314622ab52b56e7fb6b33
AhmedMenaim <ahmedmenaim8@gmail.com>
2024-05-02 19:18:44 +0300

    feat(universitiesList) 🚧 Add shared repository to support moving data from module to another.

---
+++ Invade-Task-VIPER/SharedRepository/UniversitySharedRepository.swift
@@ -0,0 +1,36 @@
+//
+//  UniversitySharedRepository.swift
+//  Invade-Task-VIPER
+//
+//  Created by Menaim on 02/05/2024.
+//
+
+import Foundation
+
+public class UniversitySharedRepository {
+  // MARK: - Properties
+
+  private var university: UniversityItem?
+
+  // MARK: - Singleton
+
+  private init() { }
+
+  public static var shared = UniversitySharedRepository()
+}
+
+// MARK: - UniversitySharedRepositoryProtocol
+
+extension UniversitySharedRepository: UniversitySharedRepositoryProtocol {
+  public func get() -> UniversityItem? {
+    UniversitySharedRepository.shared.university
+  }
+
+  public func save(product: UniversityItem) {
+    UniversitySharedRepository.shared.university = university
+  }
+
+  public func clear() {
+    university = nil
+  }
+}
---
+++ Invade-Task-VIPER/SharedRepository/UniversitySharedRepositoryProtocol.swift
@@ -0,0 +1,24 @@
+//
+//  UniversitySharedRepositoryProtocol.swift
+//  Invade-Task-VIPER
+//
+//  Created by Menaim on 02/05/2024.
+//
+
+import Foundation
+
+public protocol UniversitySharedRepositoryProtocol: UniversitySharedRepositoryGettable,
+  UniversitySharedRepositorySavable,
+  UniversitySharedRepositoryClearable { }
+
+public protocol UniversitySharedRepositoryGettable {
+  func get() -> UniversityItem?
+}
+
+public protocol UniversitySharedRepositorySavable {
+  func save(product: UniversityItem)
+}
+
+public protocol UniversitySharedRepositoryClearable {
+  func clear()
+}
